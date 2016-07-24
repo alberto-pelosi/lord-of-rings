21f97434cbc6c462349195b4837d7a0e526a2b55
albi <alberto.pelosi.work@gmail.com>
2016-07-24 14:41:03 +0200

    FIX: lord of rings's armies

--- src/main/scala/it/kabul/lor/Launcher.scala
+++ src/main/scala/it/kabul/lor/Launcher.scala
@@ -6,8 +6,8 @@ object Launcher {
   val NUMBER_OF_SOLDIER = 100
   
   def main(args: Array[String]) : Unit = {
-    var army1 = new Army("Nazi", FighterFactory.enlistArmy(NUMBER_OF_SOLDIER))
-    var army2 = new Army("USSR", FighterFactory.enlistArmy(NUMBER_OF_SOLDIER))
+    var army1 = new Army("Gondor", FighterFactory.enlistArmy(NUMBER_OF_SOLDIER))
+    var army2 = new Army("Mondor",  FighterFactory.enlistArmy(NUMBER_OF_SOLDIER))
     Battle.simulate(army1, army2)
   }
 }
